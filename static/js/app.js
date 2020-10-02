c15701476a7e86717beefa227296894de55b199b
Paul Siekmeier <siekmeierpaul@gmail.com>
2020-10-02 16:41:00 -0500

    data into table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufoIncidents) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoIncidents).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
