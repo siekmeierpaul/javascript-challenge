b2e3e49d07b22eab24401e425ca337b5623956ff
Paul Siekmeier <siekmeierpaul@gmail.com>
2020-10-02 20:56:48 -0500

    filtering by date

--- static/js/app.js
+++ static/js/app.js
@@ -2,13 +2,60 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Select the body
 var tbody = d3.select("tbody");
 
-tableData.forEach((ufoIncidents) => {
+// Fill the table with all the default data
+tableData.forEach(ufoIncidents => {
     var row = tbody.append("tr");
     Object.entries(ufoIncidents).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+        var cell = row.append("td");
+        cell.text(value);
     });
-  });
-  
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create the function to run for both events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear the table
+    tbody.html("");
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Would be good to add some checking here to make sure the value inserted by the users can be
+    //   directly compared to the datetime value from the data and clean it up if possible
+
+    // Print the value to the console
+    console.log(inputValue);
+
+    // Filter the table data down to the date entered
+    var filtered = tableData.filter(ufoIncident => ufoIncident.datetime === inputValue);
+
+    // Fill the table with the now filtered data
+    filtered.forEach((ufoIncidents) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoIncidents).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
